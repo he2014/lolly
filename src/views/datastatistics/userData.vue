c1e23530d481055bf0aaf426273eff9957380936
he2014 <>
2018-07-16 15:39:33 +0800

    00

--- src/views/datastatistics/userData.vue
+++ src/views/datastatistics/userData.vue
@@ -18,8 +18,7 @@
     <el-row :span="24">
         <el-col :span="12" id="callData" :style="{paddingRight:'10px'}">
             <el-row>
-                <!-- <el-date-picker v-model="value2" type="datetime" placeholder="选择日期时间" size="mini">
-                </el-date-picker> -->
+              
                 <el-button type="primary" size="mini" @click.stop="getCallData">刷新</el-button>
             </el-row>
 
@@ -37,10 +36,9 @@
         </el-col>
 
         <el-col :span="12" id="connectData" :style="{paddingLeft:'10px'}">
-            <!-- <el-col :style="{width:'1px',height:'100%',backgroundColor:'#409eff'}"></el-col> -->
+            
             <el-row>
-                <!-- <el-date-picker v-model="value2" type="datetime" placeholder="选择日期时间" size="mini">
-                </el-date-picker> -->
+              
                 <el-button type="primary" size="mini" @click.stop="getCallData1">刷新</el-button>
             </el-row>
 
@@ -66,11 +64,7 @@
             </el-table-column>
             <el-table-column prop="startTime" label="开始时间" width="180">
             </el-table-column>
-            <!-- <el-table-column prop="" label="">
-                <template scope="scope">
-                        <el-button type="primary" size="mini"  @click.stop="tableChange1(scope.row)">详情</el-button>
-                </template>
-            </el-table-column> -->
+         
         </el-table>
         <span slot="footer" class="dialog-footer">
     <el-button @click="dialogVisible1 = false">取 消</el-button>
@@ -88,11 +82,7 @@
             </el-table-column>
             <el-table-column prop="endTime" label="结束时间" width="180">
             </el-table-column>
-            <!-- <el-table-column prop="" label="">
-                <template scope="scope">
-                        <el-button type="primary" size="mini"  @click.stop="tableChange1(scope.row)">详情</el-button>
-                </template>
-            </el-table-column> -->
+           
         </el-table>
         <span slot="footer" class="dialog-footer">
     <el-button @click="dialogVisible2 = false">取 消</el-button>
@@ -102,106 +92,128 @@
 </section>
 </template>
 <script>
-import http from '@/api/http'
-import {
-    getTimeStap,
-    dateTime
-} from '@/common/js/public'
+import http from "@/api/http";
+import { getTimeStap, dateTime } from "@/common/js/public";
 export default {
-    data() {
-        return {
-            dialogVisible1: false,
-            dialogVisible2: false,
-            value1: new Date((new Date(new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate()).getTime()) + 8 * 60 * 60 * 1000),
-            data: {},
-            data1: [],
-            data2: [],
-            data3: [],
-            data4: [],
-            message: {
-                value1: "",
-                value2: "",
-                value3: "",
-
-            }
-        }
+  data() {
+    return {
+      dialogVisible1: false,
+      dialogVisible2: false,
+      value1: new Date(
+        new Date(
+          new Date().getFullYear() +
+            "-" +
+            (new Date().getMonth() + 1) +
+            "-" +
+            new Date().getDate()
+        ).getTime() +
+          8 * 60 * 60 * 1000
+      ),
+      data: {},
+      data1: [],
+      data2: [],
+      data3: [],
+      data4: [],
+      message: {
+        value1: "",
+        value2: "",
+        value3: ""
+      }
+    };
+  },
+  methods: {
+    timeChange() {
+      this.getUserData();
+      this.getCallData();
+      this.getCallData1();
     },
-    methods: {
-        timeChange() {
-            this.getUserData();
-            this.getCallData();
-            this.getCallData1();
+    getUserData() {
+      //console.log(this.value1)
+      this.message.value1 = this.value1;
+      let obj = Object.assign({}, this.message);
+      http.get(
+        "/back/sts/quds",
+        {
+          createTime: getTimeStap(obj.value1)
         },
-        getUserData() {
-            //console.log(this.value1)
-            this.message.value1 = this.value1;
-            let obj = Object.assign({}, this.message);
-            http.get("/back/sts/quds", {
-                createTime: getTimeStap(obj.value1)
-            }, (data) => {
-                this.data = data.dataInfo;
-            })
-            //methods
-        },
-        getCallData() {
-            this.message.value1 = this.value1;
-            let obj = Object.assign({}, this.message);
-            http.get("/back/sts/list/call", {
-                createTime: getTimeStap(obj.value1)
-            }, (data) => {
-                this.data1 = data.dataInfo;
-            })
-        },
-        tableChange1(val) {
-            this.message.value1 = this.value1;
-            let obj = Object.assign({}, this.message);
-            http.get("/back/sts/list/call/detail", {
-                createTime: getTimeStap(obj.value1),
-                hid: val.hostId
-            }, (data) => {
-                this.dialogVisible1 = true
-                let datas = data.dataInfo;
-                datas.forEach((item, key) => {
-                    datas[key].startTime = dateTime.fulltime(new Date(item.startTime))
-                })
-                this.data2 = data.dataInfo;
-
-            })
-        },
-        getCallData1() {
-            this.message.value1 = this.value1;
-            let obj = Object.assign({}, this.message);
-            http.get("/back/sts/list/connect", {
-                createTime: getTimeStap(obj.value1)
-            }, (data) => {
-                this.data3 = data.dataInfo;
-            })
-        },
-        tableChange2(val) {
-            this.message.value1 = this.value1;
-            let obj = Object.assign({}, this.message);
-            http.get("/back/sts/list/connect/detail", {
-                createTime: getTimeStap(obj.value1),
-                hid: val.hostId
-            }, (data) => {
-                this.dialogVisible2 = true
-                let datas = data.dataInfo;
-                datas.forEach((item, key) => {
-                    datas[key].startTime = dateTime.fulltime(new Date(item.startTime))
-                    datas[key].endTime = dateTime.fulltime(new Date(item.endTime))
-                })
-                this.data4 = data.dataInfo;
-
-            })
+        data => {
+          this.data = data.dataInfo;
         }
-
+      );
+      //methods
     },
-    mounted() {
-        this.getUserData();
-        this.getCallData();
-        this.getCallData1();
+    getCallData() {
+      this.message.value1 = this.value1;
+      let obj = Object.assign({}, this.message);
+      http.get(
+        "/back/sts/list/call",
+        {
+          createTime: getTimeStap(obj.value1)
+        },
+        data => {
+          this.data1 = data.dataInfo;
+        }
+      );
+    },
+    tableChange1(val) {
+      this.message.value1 = this.value1;
+      let obj = Object.assign({}, this.message);
+      http.get(
+        "/back/sts/list/call/detail",
+        {
+          createTime: getTimeStap(obj.value1),
+          hid: val.hostId
+        },
+        data => {
+          this.dialogVisible1 = true;
+          let datas = data.dataInfo;
+          datas.forEach((item, key) => {
+            datas[key].startTime = dateTime.fulltime(new Date(item.startTime));
+          });
+          this.data2 = data.dataInfo;
+        }
+      );
+    },
+    getCallData1() {
+      this.message.value1 = this.value1;
+      let obj = Object.assign({}, this.message);
+      http.get(
+        "/back/sts/list/connect",
+        {
+          createTime: getTimeStap(obj.value1)
+        },
+        data => {
+          this.data3 = data.dataInfo;
+        }
+      );
+    },
+    tableChange2(val) {
+      this.message.value1 = this.value1;
+      let obj = Object.assign({}, this.message);
+      http.get(
+        "/back/sts/list/connect/detail",
+        {
+          createTime: getTimeStap(obj.value1),
+          hid: val.hostId
+        },
+        data => {
+          this.dialogVisible2 = true;
+          let datas = data.dataInfo;
+          datas.forEach((item, key) => {
+            datas[key].startTime = dateTime.fulltime(new Date(item.startTime));
+            datas[key].endTime = dateTime.fulltime(new Date(item.endTime));
+          });
+          this.data4 = data.dataInfo;
+        }
+      );
     }
-}
+  },
+  mounted() {
+    this.getUserData();
+    this.getCallData();
+    this.getCallData1();
+  }
+};
 </script>
 <style lang="scss" scoped>
 </style>
